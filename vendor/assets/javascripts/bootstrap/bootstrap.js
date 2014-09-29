46c529e98b2862366ef8d22f8a13494abf418c46
thanh tung <tungtk1993@gmail.com>
2014-09-28 22:27:05 -0700

    Lesson 5

---
+++ vendor/assets/javascripts/bootstrap/bootstrap.js
@@ -0,0 +1,13 @@
+//=require ./affix
+//=require ./alert
+//=require ./carousel
+//=require ./button
+//=require ./dropdown
+//=require ./carousel
+//=require ./collapse
+//=require ./modal
+//=require ./popover
+//=/require ./scrollspy
+//=require ./tab
+//=require ./tooltip
+//=require ./transition
